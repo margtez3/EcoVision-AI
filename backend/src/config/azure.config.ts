015dbf7a06e290d7db440ab76b86d6ed64fa4ced
AlejandroBR10 <alejabaldera@gmail.com>
2026-05-22 11:20:01 -0600

    src-azureconfig/adding instructions

---
+++ backend/src/config/azure.config.ts
@@ -0,0 +1,11 @@
+/*
+
+--AGREGAR CONFIGURACION DE AZURE
+
+--VARIABLES DE ENTORNO EN .env YA SE LA SABEN, los que tengan que usarlo suban el .ev.example para ver que variables agregar 
+--LOS DE BACKEND SIGAN LA ESTRUCTURA ADECUADA (todo en la carpeta src y agregen sus propias carpetas
+segun sean necesarias ej: src/routes/routes.ts)
+etc....
+--
+
+*/
